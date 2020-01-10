2aff0f0fedd1a169c0e7b822c3a67d8acf985c41
Alexander Polyansky <aplsk74@gmail.com>
2020-01-10 20:09:01 +0300

    Add template

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
+import { scss } from '@kazzkiq/svelte-preprocess-scss';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -20,6 +21,9 @@ export default {
 			dev: !production,
 			// we'll extract any component CSS out into
 			// a separate file — better for performance
+			preprocess: {
+        style: scss(),
+      },
 			css: css => {
 				css.write('public/build/bundle.css');
 			}
